e16c8f26665a84221abfdfc604c94d8160e360b3
MarkoM91 <markomedic91@gmail.com>
2019-05-22 19:00:46 +0200

    commit3

--- resources/views/page/home.blade.php
+++ resources/views/page/home.blade.php
@@ -4,9 +4,9 @@
 
 @foreach($paganti as $pagante)
   <div class="box-container">
-      <div class="box">{{ $pagante -> id }}</div>
-      <div class="box">{{ $pagante -> name }}</div>
-      <div class="box">{{ $pagante -> lastname }}</div>
+      <div class="box line_height">{{ $pagante -> id }}</div>
+      <div class="box line_height">{{ $pagante -> name }}</div>
+      <div class="box line_height">{{ $pagante -> lastname }}</div>
       <div class="box">{{ $pagante -> address }}</div>
   </div>
   @endforeach
