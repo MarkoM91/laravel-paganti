fe3e28433da28c31f1c1bb0ba7d133043d1249ae
MarkoM91 <markomedic91@gmail.com>
2019-05-22 18:52:48 +0200

    commit2

--- resources/views/page/home.blade.php
+++ resources/views/page/home.blade.php
@@ -1,13 +1,15 @@
 @extends('layout.home-layout')
 
 @section('content')
-  @foreach($paganti as $pagante)
 
+@foreach($paganti as $pagante)
   <div class="box-container">
-      <div class="box">{{  }}</div>
-      <div class="box">{{  }}</div>
-      <div class="box">{{  }}</div>
-      <div class="box">{{  }}</div>
+      <div class="box">{{ $pagante -> id }}</div>
+      <div class="box">{{ $pagante -> name }}</div>
+      <div class="box">{{ $pagante -> lastname }}</div>
+      <div class="box">{{ $pagante -> address }}</div>
   </div>
   @endforeach
+
+
 @stop
